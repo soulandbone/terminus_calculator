db70378d78722b7acbcf4bd0a8bb1e6c41316b1f
Gustavo G <soulandbone@gmail.com>
2025-03-28 11:52:13 -0300

    now we only use one provider and we derive coordinates by using a getter, no need to create a second provider

--- lib/providers/index_provider.dart
+++ lib/providers/index_provider.dart
@@ -5,6 +5,14 @@ class IndexNotifier extends StateNotifier<List<int>> {
 
   List<int> values = [0, 10, 11, 20, 21, 22];
 
+  List<int> get coordinates {
+    return [
+      2 * values[state[0]] + 11,
+      2 * values[state[2]] + values[state[1]] - 5,
+      (values[state[2]] + values[state[1]] - values[state[0]]).abs(),
+    ];
+  }
+
   void updateIndex(int selectedIndex, String letter) {
     List<int> updatedList = List.from(state);
     switch (letter) {
@@ -18,7 +26,9 @@ class IndexNotifier extends StateNotifier<List<int>> {
         updatedList = [state[0], state[1], selectedIndex];
         break;
     }
+
     state = updatedList;
+
     print(state);
   }
 }
--- lib/widgets/result_box.dart
+++ lib/widgets/result_box.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:terminus_app/providers/calculation_provider.dart';
+import 'package:terminus_app/providers/index_provider.dart';
 
 import 'package:terminus_app/widgets/result_item.dart';
 
@@ -9,21 +9,20 @@ class ResultBox extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final provider = ref.watch(calculationProvider);
+    ref.watch(selectedIndexProvider); //we watch for the indexes to update
+    var coordinates =
+        ref
+            .watch(selectedIndexProvider.notifier)
+            .coordinates; //we get the coordinates info
     return Container(
       decoration: BoxDecoration(color: Colors.green.withValues(alpha: 0.2)),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          ResultItem(
-            provider[0],
-          ), //first index of the state, representing the first calculated coordinate (that depends on the index State of x ) 2x+11
-          ResultItem(
-            provider[1],
-          ), //  //second index of the state, representing the second calculated coordinate (that depends on the index State of z and y) 2z+y -5
-          ResultItem(
-            provider[2],
-          ), // third index of the state, representing the third calculated coordinate (that depends on the index State of z, y and x) |y+z -x|
+          ResultItem(coordinates[0]),
+          ResultItem(coordinates[1]),
+          ResultItem(coordinates[2]),
+          //
         ],
       ),
     );
